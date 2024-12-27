4af3835267d99b5e84cc6a604b704d3dd61e3c17
Krzysztof Poskart <krzysztof.poskart@ing.com>
2024-12-27 21:59:48 +0100

    well done

---
+++ Python/listy.py
@@ -0,0 +1,17 @@
+# mozna je zadeklaorwac na 2 sposoby
+# mozna je odwracac, sortowac latwo i szybko przeszukiwac
+
+lista=[]
+lista2=list()
+
+# obie beda puste ale znacza to samo
+
+lista3=[1,2,3,"baba jaga patrzy"]
+lista4=["nie","toperz",123,lista3]
+
+# listy moga zawierac elementy dowolnych rodzajow i typow
+
+print(lista4)
+# mozna je wyswietlac i to bedzie takie cos:
+# ['nie', 'toperz', 123, [1, 2, 3, 'baba jaga patrzy']]
+
--- Python/string.py
+++ Python/string.py
@@ -52,7 +52,61 @@ print(napis.split(";"))
 
 # Łancuchy funkcji
 
-napis="to ja Krzysztof Rafał Tomasz Poskart"
-print(napis.strip("to").title().replace('Poskart','Bakoma'))
+napis="toja krzysztof poskar"
+print(napis.strip("to").title().replace("P","X"))
 # pokaze:  Ja Krzysztof Rafał Tomasz Poskar
 
+# iteracja po lancuchach tekstowych
+nazwa="dupadupa123zglossie"
+for i in nazwa:
+    print(i)
+# wyswietli dupadupa123zglossie w koluemnie po jednym znaku
+
+
+# mnozenie tekstu - powtorzenie danej frazy wiele razy
+kriszna= "rama "*5+" "+5*"hare"
+print(kriszna)
+# wyswietli:
+# rama rama rama rama rama  hareharehareharehare
+
+# sprawdzanie tekstu czy zawiera jakas fraze
+if ("X" in "SpaceX"):
+    print('ten ciag znakow zawiera X!')
+# z racji ze zabiera to wyswietli to co jest w print
+# zamiast X moze byc dowolna fraza nie tylko jeden znak
+
+# taki smaczek z ksiazki
+
+if("Python">"Java"):
+    print("to chyba jasne :) Tu jest miejsce na hejt: ........")
+else:
+    print("coś się spsuło...")
+#wyswietli: to chyba jasne :) Tu jest miejsce na hejt: ........
+# bo jakbys alfabetycznie te slowa uporzadkowal to Python bedzie po Javie :)
+
+# ciecie lancuchow tekstowych.
+
+lancuch='123456789'
+print(lancuch[2])
+# pokaze 3 bo w pythonie liczy od zera, czyli nie pokaze 1 ani 2-ki
+# bo 1 to zerom 2 to jeden i 3 na liscie to 2 w lancuchu
+print(lancuch[-2])
+# pokaze 8 bo to drugi znak od konca 
+
+print(lancuch[2:5])
+# pokaze zakres: 345  
+
+print(lancuch[:5])
+# pokaze Isze 5 znakow: 12345
+
+print(lancuch[:-3])
+# lub wytnie ciag bez ostatnich 3 znakow od konca czyli pokaze 123456
+
+print(lancuch[0:6:2])
+# lub przeskakiwac co jakis znak - co dwa znaki od zerowej pozycji do 7mego: 135
+
+print(lancuch[0:len(lancuch):2])
+# np wyciecie co drugi znak z calego tekstu da 13579
+
+print(lancuch[::2])
+# to samo co powyzej -> 13579 ale w krotszy sposob
